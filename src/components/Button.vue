66b743c3adacbffe5994031f86f2e834c993104e
Fabio <alessf13@gmail.com>
2021-09-12 17:29:40 +0200

    feat: onClick method added

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,20 @@
 <template>
-  <button class="btn">Add Task</button>
+  <button @click="onClick()" class="btn" :style="{ background: color }">
+    {{ text }}
+  </button>
 </template>
 
 <script>
 export default {
   name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log("click");
+    },
+  },
 };
 </script>
